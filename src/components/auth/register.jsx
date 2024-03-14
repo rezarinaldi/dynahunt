4afe2e064ad192dd61a86c16f84abdeb76aa7aaa
rezarinaldi <rez4rinaldi@gmail.com>
2024-03-14 22:42:51 +0700

    fix: add type input & regenarete prisma

--- src/components/auth/register.jsx
+++ src/components/auth/register.jsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 export const Register = () => {
   const [registerData, setRegisterData] = useState({
     nama: "",
-    phone_number: "",
+    phone_number: 0,
     email: "",
     password: "",
   });
@@ -14,9 +14,9 @@ export const Register = () => {
   }
 
   async function handleRegister() {
-    const { name, email, password } = registerData;
+    const { name, phone_number, email, password } = registerData;
 
-    if (!name || !email || !password) {
+    if (!name || !phone_number || !email || !password) {
       console.log("All fields must be filled");
       return;
     }
@@ -36,18 +36,26 @@ export const Register = () => {
         <h1>Register</h1>
       </div>
       <div className="grid grid-cols-4 gap-4">
-        <input name="name" placeholder="Name" onChange={handleInputEvent} />
         <input
+          type="name"
+          name="name"
+          placeholder="Name"
+          onChange={handleInputEvent}
+        />
+        <input
+          type="number"
           name="phone_number"
           placeholder="Phone Number"
           onChange={handleInputEvent}
         />
         <input
+          type="email"
           name="email"
           placeholder="email@domain.com"
           onChange={handleInputEvent}
         />
         <input
+          type="password"
           name="password"
           placeholder="password"
           onChange={handleInputEvent}
