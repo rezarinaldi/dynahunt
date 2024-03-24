e18a585321bedbf21967be75fa870edb2eca10d7
Karya Lingga <usscharlesberry@gmail.com>
2024-03-24 08:07:32 +0700

    update home and profile page

--- src/components/influencer/EmbedItem.jsx
+++ src/components/influencer/EmbedItem.jsx
@@ -4,15 +4,19 @@ export const EmbedItem = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4 md:grid-cols-2 gap-4">
       <InstagramEmbed
-        url="https://www.instagram.com/reel/C41SqU8yqW3/?igsh=aXo4dndjenNjeGdu"
+        url="https://www.instagram.com/reel/CxFoPSuS6-5/"
         width={328}
       />
       <InstagramEmbed
-        url="https://www.instagram.com/reel/C41SqU8yqW3/?igsh=aXo4dndjenNjeGdu"
+        url="https://www.instagram.com/reel/CwOn9BQAy_U/"
         width={328}
       />
       <InstagramEmbed
-        url="https://www.instagram.com/reel/C41SqU8yqW3/?igsh=aXo4dndjenNjeGdu"
+        url="https://www.instagram.com/reel/CxCP-aQyBe4/"
+        width={328}
+      />
+      <InstagramEmbed
+        url="https://www.instagram.com/reel/C3F0o8uPx3J/"
         width={328}
       />
     </div>
--- src/components/influencer/ProfileItem.jsx
+++ src/components/influencer/ProfileItem.jsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 export const ProfileItem = () => {
   return (
     <div className="text-center mb-4 bg-slate-200 rounded-2xl p-4">
-      <p className="text-xl font-semibold">Zayn Malik</p>
+      <p className="text-xl font-semibold">Pevita Pearce</p>
       <div className="my-2 flex gap-1 justify-center">
-        <p className="text-sm lg:text-md font-bold">12K</p>
+        <p className="text-sm lg:text-md font-bold">18.3M</p>
         <p className="text-sm lg:text-md">Followers</p>
       </div>
       <div className="flex justify-center">
         <div className="avatar">
           <div className="w-48 rounded-full">
             <Image
-              src={"/img/campaign/zayn.jpeg"}
+              src={"/img/fotoprofil/pev.jpg"}
               alt="profil"
               width={200}
               height={200}
@@ -21,13 +21,19 @@ export const ProfileItem = () => {
           </div>
         </div>
       </div>
-      <p className="text-md lg:text-sm font-semibold">Fashion</p>
-      <p className="text-md lg:text-sm my-2">
-        Zain Javadd Malik adalah seorang penyanyi dan penulis lagu berkebangsaan
-        Inggris. Pada tahun 2010, Malik mengikuti audisi sebagai artis solo
-        untuk serial televisi Inggris The X Factor.
+      <div className="flex justify-center mt-3 mb-2 gap-2">
+        <div className="badge badge-outline">Fashion</div>
+        <div className="badge badge-outline">Beuaty</div>
+        <div className="badge badge-outline">Sport</div>
+      </div>
+      <p className="text-md lg:text-sm mb-1">
+        Pevita Cleo Eileen Pearce adalah pemeran dan model Indonesia keturunan
+        Wales dan Banjar. Ia dikenal luas berkat perannya dalam film 5 cm dan
+        Tenggelamnya Kapal Van der Wijck.
       </p>
-      <p className="text-md lg:text-sm my-2">Bradford, Britania Raya</p>
+      <p className="text-md lg:text-sm mb-2 font-semibold">
+        Jakarta, DKI Jakarta
+      </p>
     </div>
   );
 };
